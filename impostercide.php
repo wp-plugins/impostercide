4ae4be4f0d51509f983fcaa5d330a67110f8c11d
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-03 19:35:40 +0000

    GPL2
    git-svn-id: https://plugins.svn.wordpress.org/impostercide/trunk@446881 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- impostercide.php
+++ impostercide.php
@@ -14,7 +14,7 @@ Copyright 2010-11 Mika Epstein (email: ipstenu@ipstenu.org)
 
     Impostercide is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
     Impostercide is distributed in the hope that it will be useful,
