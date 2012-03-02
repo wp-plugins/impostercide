9acf0b58a1b09d1df6c70591263474e4903a6fe6
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-02 04:08:06 +0000

    tweaking links
    git-svn-id: https://plugins.svn.wordpress.org/impostercide/trunk@513331 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- impostercide.php
+++ impostercide.php
@@ -3,7 +3,7 @@
 Plugin Name: Impostercide
 Plugin URI: http://tech.ipstenu.org/my-plugins/impostercide/
 Description: Impostercide prevents unauthenticated users from "signing" a comment with a registered users' email address.
-Version: 1.7
+Version: 1.8
 Author: Mika Epstein, Scott Merrill
 Author URI: http://www.ipstenu.org/
 
