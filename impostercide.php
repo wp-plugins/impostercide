dab94f435942ee8b2de37f8e4f07ffe94a9bd2ad
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-17 17:21:13 +0000

    Languages are good to have!
    git-svn-id: https://plugins.svn.wordpress.org/impostercide/trunk@532400 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- impostercide.php
+++ impostercide.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: Impostercide
-Plugin URI: http://tech.ipstenu.org/my-plugins/impostercide/
+Plugin URI: http://halfelf.org/plugins/impostercide/
 Description: Impostercide prevents unauthenticated users from "signing" a comment with a registered users' email address.
 Version: 1.8
 Author: Mika Epstein, Scott Merrill
 Author URI: http://www.ipstenu.org/
+Network: true
 
 Copyright 2005-07 Scott Merrill (skippy@skippy.net)
 Copyright 2010-11 Mika Epstein (email: ipstenu@ipstenu.org)
@@ -26,10 +27,17 @@ Copyright 2010-11 Mika Epstein (email: ipstenu@ipstenu.org)
     along with WordPress.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-add_filter ('preprocess_comment', 'sdm_protect_email');
 
-if (! function_exists ('sdm_protect_email')) :
-function sdm_protect_email ($data) {
+// Internationalization
+add_action( 'init', 'ippy_impostercide_internationalization' );
+function ippy_impostercide_internationalization() {
+	load_plugin_textdomain('ippy_impostercide', false, 'impostercide/languages' );
+}
+
+add_filter ('preprocess_comment', 'impostercide_protect_email');
+
+if (! function_exists ('impostercide_protect_email')) :
+function impostercide_protect_email ($data) {
 global $wpdb, $user_ID, $user_login, $user_email;
 
 extract ($data);
@@ -46,13 +54,15 @@ if ( is_user_logged_in() ) {
         return $data;
 }
 
-$imposter_message = '<h2>Possible Imposter</h2> <p>You are attempting to post a comment with information (i.e. email address or login ID) belonging to a registered user. If you have an account on this site, please login to make your comment. Otherwise, please try again with different information.</p>';
+$imposter_message = '<h2>' . __('Possible Imposter', 'ippy_impostercide') .'</h2> <p>' . __('You are attempting to post a comment with information (i.e. email address or login ID) belonging to a registered user. If you have an account on this site, please login to make your comment. Otherwise, please try again with different information.', 'ippy_impostercide') .'</p>';
+
+$imposter_error = __('Error: Imposter Detected');
 
 // a name was supplied, so let's check the login names
 if ('' != $comment_author) {
         $result = $wpdb->get_var("SELECT count(ID) FROM $wpdb->users WHERE user_login='$comment_author'");
         if ($result > 0) {
-			wp_die( __( $imposter_message), 'Error: Imposter Detected' );
+			wp_die( $imposter_message, $imposter_error );
         }
 }
 
@@ -60,7 +70,7 @@ if ('' != $comment_author) {
 if ('' != $comment_author_email) {
         $result = $wpdb->get_var("SELECT count(ID) FROM $wpdb->users WHERE user_email='$comment_author_email'");
         if ($result > 0) {
-			wp_die( __( $imposter_message), 'Error: Imposter Detected' );
+			wp_die( $imposter_message, $imposter_error );
 		}
 }
 
@@ -77,6 +87,3 @@ function impostercide_donate_link($links, $file) {
         }
         return $links;
 }
-
-
-?>
--- readme.txt
+++ readme.txt
@@ -2,22 +2,27 @@
 Contributors: Ipstenu, skippy
 Tags: comments, spoof, imposter, multisite, wpmu
 Requires at least: 2.1
-Tested up to: 3.3
-Stable tag: 1.7
+Tested up to: 3.4
+Stable tag: 1.8
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5227973
 
-Impostercide prevents unauthenticated users from "signing" a comment with a registered users' email address.
+Prevent unauthenticated users from using a registered users' credentials when commenting.
 
 == Description ==
 
-Impostercide prevents unauthenticated users from "signing" a comment with a registered users email address, name or URL.  There is no interface from the admin's end, no options to select.  Simply install, turn it on, and watch the spoofers get stopped.
+Impostercide prevents unauthenticated users from "signing" a comment with a registered users email address or username.  There is no interface from the admin's end, no options to select.  Simply install, turn it on, and watch the spoofers get stopped.
 
 **Misc**
 
+* [Plugin Site](http://halfelf.org/plugins/impostercide/)
 * [Donate](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5227973)
-* [Plugin Site](http://tech.ipstenu.org/my-plugins/impostercide/)
 
 ==Changelog==
+
+= 1.8 = 
+* 17 April, 2012 by Ipstenu
+* Readme cleanup, fixing URLs etc.
+* Internationalization
 
 = 1.7 =
 * 4 October 2011 by Ipstenu
@@ -65,7 +70,6 @@ Again, yes.  I suggest putting it in `mu-plugins`, but it works fine both ways.
 = Can this catch innocents? =
 
 Yes, but ... well. The only person I've caught is someone who is, apparently, pathalogically incapable of remembering that he HAS an account on the site, and he needs to LOG IN with said account. He also claims to forget his password and that WordPress doesn't email it to him, so I'm pretty much chucking his complaint as a problem with the user, and not the tool.
-
 
 == Screenshots ==
 1. Error message when you try and post as a registered user
